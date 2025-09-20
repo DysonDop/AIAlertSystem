b36c9b735a11272c75584a0966eea210fb385830
Josh5620 <josh.ong2005@gmail.com>
2025-09-20 16:18:30 +0800

    Revert "WIP: sync local changes"
    This reverts commit 1f662dd2572f6e444ca4c3ce6c5c1c8f7ac61260.

--- client/src/services/amplify.js
+++ client/src/services/amplify.js
@@ -1,2 +1,30 @@
-// Mock amplify service - not using AWS Amplify
-export default {};
+import { Amplify } from 'aws-amplify';
+
+const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COGNITO_APP_CLIENT_ID,
+      region: import.meta.env.VITE_AWS_REGION,
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: 'code',
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+};
+
+Amplify.configure(amplifyConfig);
